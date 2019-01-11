3a3d2fabdf6648ae366fc1b762cde7a6561067c4
Aggie Koko <agata.kosior57@gmail.com>
2019-01-11 15:53:41 +0100

    Final version

--- src/modules/checkbox/index.js
+++ src/modules/checkbox/index.js
@@ -2,18 +2,41 @@
 import React from 'react';
 
 class Checkbox extends React.Component {
-    render() {
-        return(
-            <div className="checkbox">
-                <div className="title">
-                    Checkbox
-                </div>
-                <div className="content">
-                    <input type="checkbox" />
-                </div>
-            </div>
-        );
-    }
+  constructor() {
+    super();
+    this.state = {
+      checked: false
+    };
+  }
+  handleChange = () => {
+    this.setState({ checked: !this.state.checked });
+  };
+  render() {
+    const { checked } = this.state;
+    return (
+      <div className="checkbox">
+        <div className="title">Checkbox</div>
+        <div className="content">
+          <input
+            type="checkbox"
+            id="check"
+            className="checkbox__input"
+            checked={this.state.checked}
+            onChange={this.handleChange}
+          />
+          <label
+            htmlFor="check"
+            className={
+              checked ? 'input-wrapper input-wrapper--checked' : 'input-wrapper'
+            }
+          >
+            <div className="input-check" />
+          </label>
+        </div>
+        {checked && <div className="hidden-content">hidden content</div>}
+      </div>
+    );
+  }
 }
 
 export default Checkbox;
